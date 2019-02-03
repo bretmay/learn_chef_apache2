02e25f4578de250b00425a0a61eba6748f1a3078
Bret May <bret.may@gmail.com>
2019-02-02 20:59:35 -0700

    added a new template

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,18 @@
+apt_update 'Update the apt cache daily' do
+  frequency 86_400
+  action :periodic
+end
+
+package 'apache2'
+
+service 'apache2' do
+  supports status: true
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
 #
 # Cookbook:: learn_chef_apache2
 # Recipe:: default
